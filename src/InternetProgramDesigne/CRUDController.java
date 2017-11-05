f5f86b9a29434d3cc31d442e749bb39d2ae8e4b7
ClasAndExit <778379861@qq.com>
2017-11-05 15:20:38 +0800

    Change-Id: I767ee410827bca2536b3aae4928ea3a53a4df14d Signed-off-by: ClasAndExit <778379861@qq.com>

--- src/InternetProgramDesigne/CRUDController.java
+++ src/InternetProgramDesigne/CRUDController.java
@@ -4,58 +4,89 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CRUDController {
-	List <Student> student =new ArrayList<Student>();
+	List<Student> student = new ArrayList<Student>();
+
+	public List<Student> returnList(){
+		return student;
+	}
 	/**
 	 * 向List集合里面添加一个学生的信息
+	 * 
 	 * @param stu
 	 * @return
 	 */
-	public boolean addStu(Student stu){
-		boolean result=false;
-		if(student.add(stu)){
-			result=true;
+	public boolean addStu(Student stu) {
+		boolean result = false;
+		if (student.add(stu)) {
+			result = true;
 		}
 		return result;
 	}
+
 	/**
 	 * 添加学生到指定位置
+	 * 
 	 * @param index
 	 * @param stu
 	 * @return
 	 */
-	public boolean addStu(int index,Student stu){
-		boolean result=false;
-		student.add(index,stu);
+	public boolean addStu(int index, Student stu) {
+		boolean result = false;
+		student.add(index, stu);
 		return result;
 	}
+
 	/**
 	 * 根据学号查找学生
+	 * 
 	 * @param stu
 	 * @return
 	 */
-	public Student searchStu(Student stu){
-		
-		
-		return stu;
+	public Student searchStu(String stu) {
+		for (Student who : student) {
+			if (who.getStu_code().equals(stu)) {
+				return who;
+			}
+		}
+		return null;
 	}
+
 	/**
 	 * 更改学生信息
+	 * 
 	 * @param stu
 	 * @return
 	 */
-	public boolean updateStu(Student stu){
-		boolean result=false;
-		
+	public boolean updateStu(Student stu) {
+		boolean result = false;
+		for (Student who : student) {
+			if (who.getStu_code().equals(stu.getStu_code())) {
+				who.setStu_name(stu.getStu_name());
+				who.setStu_age(stu.getStu_age());
+				who.setStu_gender(stu.getStu_gender());
+				result=true;
+			}
+		}
 		return result;
 	}
+
 	/**
 	 * 删除查找到的学生
+	 * 
 	 * @param stu
 	 * @return
 	 */
-	public boolean deleteStu(Student stu){
-		boolean result=false;
-		
+	public boolean deleteStu(String stu) {
+		boolean result = false;
+		for (int i = 0, len = student.size(); i < len; ++i) {
+			if (student.get(i).getStu_code().equals(stu)) {
+				if (student.remove(i)!=null) {
+					result=true;
+				}
+				--len;// 减少一个
+				--i;
+			}
+		}
 		return result;
 	}
 }
--- src/InternetProgramDesigne/Test2.java
+++ src/InternetProgramDesigne/Test2.java
@@ -1,27 +1,64 @@
 package InternetProgramDesigne;
-
+import java.util.Scanner;
 public class Test2 {
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("klasdfklajsklfgasjfkla");
 		CRUDController con = new CRUDController();
+		System.out.println("學生信息列表：");
+		for(Student student : con.returnList()) {
+			printStudent(student);
+		}
+		
 		Student s = credateStudent();
 		if(con.addStu(s)){
 			System.out.println("添加成功");
 			printStudent(s);
 		}
 		
+		System.out.println("输入学生的学号查询学生信息：");
+		Scanner scan = new Scanner (System.in);
+		String searchstucode=scan.nextLine();
+		if(con.searchStu(searchstucode)==null) {
+			System.out.println("该学生不存在！");
+		}else {
+			printStudent(con.searchStu(searchstucode));
+		}
+		
+		System.out.println("输入学生的信息在记录中修改该學號学生的信息：");
+		Student stu = credateStudent();
+		if(con.updateStu(stu)) {
+			System.out.println("修改成功！");
+			printStudent(stu);
+		}else {
+			System.out.println("修改失败！");
+		}
+		
+		System.out.println("输入学生的学号在记录中删除该学生：");
+		String deletestucode=scan.nextLine();
+		if(con.deleteStu(deletestucode)) {
+			System.out.println("删除成功！");
+		}else {
+			System.out.println("删除失败！");
+		}
 	}
 	public static Student credateStudent(){
 		Student s = new Student();
-		s.setStu_name("王二小");
-		s.setStu_code("202020202");
-		s.setStu_age(22);
-		s.setStu_gender("man");
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("输入学生姓名:");
+		String stuname=scan.nextLine();
+		System.out.println("输入学生学号:");
+		String stucode=scan.nextLine();
+		System.out.println("输入学生性别:");
+		String stugender=scan.nextLine();
+		System.out.println("输入学生年龄:");
+		int stuage=scan.nextInt();
+		s.setStu_name(stuname);
+		s.setStu_code(stucode);
+		s.setStu_age(stuage);
+		s.setStu_gender(stugender);
 		return s;
 	}
 	public static void printStudent(Student s){
