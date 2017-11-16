0c9836e8a1e2f64441f3a82728d541b10673ef27
ClsAndExit <778379861@qq.com>
2017-11-16 20:15:39 +0800

    Change-Id: I8bc1860ebffa920c484215a41bf6e72d44eff5d2 Signed-off-by: ClsAndExit <778379861@qq.com>

--- src/InternetProgramDesigne/CRUDFromFile.java
+++ src/InternetProgramDesigne/CRUDFromFile.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
@@ -18,38 +17,77 @@ import java.util.List;
 public class CRUDFromFile {// 所有对数据的修改操作都在List里面完成，此类只提供文件读取和写入的功能
 	static List<Student> student1 = new ArrayList<Student>();
 	// 找到目标文件
-	public static File file = new File("D:\\test.txt");
+	public static File file = new File("D:\\tets.txt");
 
-	public List<Student> returnList1() throws ClassNotFoundException, IOException {
+	public List<Student> returnList1() throws ClassNotFoundException,
+			IOException {
 		OutputTheFile();
 		return student1;
 	}
 
+	// 判断文件是否存在
+	public static boolean judeFileExists() {
+		boolean fileExist = false;
+		if (file.exists()) {
+			System.out.println("找到目标文件！");
+			fileExist = true;
+		} else {
+			System.out.println("目标文件不存在，正在创建 ...");
+			try {
+				file.createNewFile();
+				fileExist = true;
+				System.out.println("文件创建成功，开始你的表演！");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return fileExist;
+	}
+
 	// 建立数据的输入通道
 	static void InputTheFile() throws IOException {
-		// 建立文件与数据的关系 并且将其序列化
-		FileOutputStream fos = new FileOutputStream(file,false);
-		ObjectOutputStream oos = new ObjectOutputStream(fos);
-		oos.writeObject(student1);
-		System.out.println("信息成功地写入了文件中！");
-		oos.close();
-		fos.close();
+		if (judeFileExists()) {
+			// 建立文件与数据的关系 并且将其序列化
+			FileOutputStream fos = new FileOutputStream(file, false);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			try{
+				oos.writeObject(student1);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				oos.close();
+				fos.close();
+			}
+			System.out.println("信息成功地写入了文件中！");
+		}else{
+			System.out.println("信息寫入失敗！");
+		}
 	}
 
 	// 只在程序开始的时候调用，用来展示文件里面原有的信息
 	static void OutputTheFile() throws IOException, ClassNotFoundException {
-		FileInputStream fis = new FileInputStream(file);
-		ObjectInputStream ois = new ObjectInputStream(fis);
-		try {
-			while (true) {
-				student1 = (List<Student>) ois.readObject();
+		if(judeFileExists()){
+			FileInputStream fis = new FileInputStream(file);
+			if (file.exists() && file.length() == 0) {
+				System.out.println("文件为空！");
+			} else {
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				try {
+					while (true) {
+						
+						student1 = (List<Student>) ois.readObject();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					ois.close();
+					fis.close();
+				}
 			}
-		} catch (Exception e) {
-		} finally {
-			ois.close();
-			fis.close();
+			System.out.println("文件读取完成，以下為現有學生信息！");
+			}else{
+				System.out.println("文件读取失敗！");
 		}
-		System.out.println("文件读取成功！");
 	}
 
 	/**
@@ -64,7 +102,6 @@ public class CRUDFromFile {// 所有对数据的修改操作都在List里面完成，此类只提供文
 			try {
 				InputTheFile();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			result = true;
@@ -100,6 +137,11 @@ public class CRUDFromFile {// 所有对数据的修改操作都在List里面完成，此类只提供文
 				who.setStu_name(stu.getStu_name());
 				who.setStu_age(stu.getStu_age());
 				who.setStu_gender(stu.getStu_gender());
+				try {
+					InputTheFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				result = true;
 			}
 		}
@@ -117,6 +159,11 @@ public class CRUDFromFile {// 所有对数据的修改操作都在List里面完成，此类只提供文
 		for (int i = 0, len = student1.size(); i < len; ++i) {
 			if (student1.get(i).getStu_code().equals(stu)) {
 				if (student1.remove(i) != null) {
+					try {
+						InputTheFile();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 					result = true;
 				}
 				--len;// 减少一个
