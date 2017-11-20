3def5694dc3a03e90c29cf6bb545cf903a2d90dd
ClsAndExit <778379861@qq.com>
2017-11-20 10:21:10 +0800

    Change-Id: I2dd77b167b4a4531c186881ff38750d2b77df0df Signed-off-by: ClsAndExit <778379861@qq.com>

--- src/javaExamp/Test10.java
+++ src/javaExamp/Test10.java
@@ -1,6 +1,7 @@
 /**
 1.	位置类Location
-设计一个名为Location的类，定位二维数组中的最大值及其位置。这个类包括公共的数据域row、column和maxValue，二维数组中的最大值及其下标用int型的row和column以及double型的maxValue存储。
+设计一个名为Location的类，定位二维数组中的最大值及其位置。这个类包括公共的数据域row、column和maxValue，
+二维数组中的最大值及其下标用int型的row和column以及double型的maxValue存储。
 编写下面的方法，返回一个二维数组中的最大值的位置。
 public static Location locateLargest(double[][] a)
 返回值是一个Location的实例。编写一个测试程序，提示用户输入一个二维数组，然后显示这个数组中的最大元素。
