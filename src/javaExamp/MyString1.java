0a6783a116bcca6e22dba2c5086a078c705c923a
ClasAndExit <778379861@qq.com>
2017-11-26 15:52:35 +0800

    Change-Id: I2f0ac09f6a7d46023a359a88608cbc132eb4eef2 Signed-off-by: ClasAndExit <778379861@qq.com>

--- src/javaExamp/MyString1.java
+++ src/javaExamp/MyString1.java
@@ -1,19 +1,43 @@
 package javaExamp;
+
 /* 
  2.	实现String类
  Java库中提供了String类。给出你自己对下面方法的实现（将新类命名为MyString1）：
- public MyString1(char[] chars);
- public char charAt(int index);
- public int length();
- public MyString1 substring(int begin, int end);
+ --public MyString1(char[] chars);
+ --public char charAt(int index);
+ --public int length();
+ --public MyString1 substring(int begin, int end);
  public static MyString1 valueOf(int i);
  */
 public class MyString1 {
 	private char[] stringChar;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 测试数据
+		char[] string = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'd', '!' };
+		MyString1 string1 = new MyString1(string);
+		// 输出字符串
+		print(string1);
+		// 输出位置为9的字符
+		char ch = string1.charAt(9);
+		System.out.println(ch);
+		// 输出字符串的长度
+		int length = string1.length();
+		System.out.println(length);
+		// 截取字符串当中的一段3~7
+		MyString1 string2 = string1.substring(3, 7);
+		print(string2);
+		// 将int类型的数据212121214转化为MyString输出
+		MyString1 string3 = MyString1.valueOf(212121214);
+		print(string3);
+	}
 
+	public static void print(MyString1 string) {
+		char[] chars = string.stringChar;
+		for (int i = 0; i < string.length(); i++) {
+			System.out.print(chars[i]);
+		}
+		System.out.println();
 	}
 
 	public MyString1(char[] chars) {
@@ -39,14 +63,21 @@ public class MyString1 {
 		MyString1 newString = new MyString1(string);
 		return newString;
 	}
-//	 public static MyString1 valueOf(int i){
-//		 //https://www.cnblogs.com/lxcmyf/p/6427972.html
-//		 if (i == Integer.MIN_VALUE)
-//		        return "-2147483648";
-//		    int size = (i < 0) ? stringSize(-i) + 1 : stringSize(i);
-//		    char[] buf = new char[size];
-//		    getChars(i, size, buf);
-//		    return new String(buf, true);
-//	 }
-}
 
+	public static MyString1 valueOf(int i) {
+		int j;
+		//，在計算數字的位數的時候會把數字清零，所以，先將數據的值保存下來
+		int value=i;
+		for (j = 0; i > 0; j++) {
+			i = i / 10;
+		}
+		char[] string = new char[j];
+		for (int m = j - 1; value > 0; m--) {
+			// chat(int) 会自动的把int类型的数据转化为ASCII码表示，所以要加上ASCII码中表示数字“０”的位数
+			string[m] = (char) (value % 10 + 48);
+			value = value / 10;
+		}
+		MyString1 newString = new MyString1(string);
+		return newString;
+	}
+}
---
+++ src/javaExamp/Test11.java
@@ -0,0 +1,21 @@
+/**
+1.	二进制转换为十进制
+编写一个方法，将一个二进制数字作为一个字符串转换为一个十进制整数。这个方法头如下所示：
+public static int binaryToDecimal(String binaryString)
+2.	删除文本
+编写一个程序，从一个文本文件中删除掉所有出现某个指定字符串的地方。例如调用
+java Exercise9 John filename
+则从指定文件中删除掉字符串John。
+
+ */
+package javaExamp;
+
+public class Test11 {
+	
+	
+	public static void main(String [] args) {
+		
+	}
+	
+	
+}
